7e8b0c24a1802b5af4cd648e85a2dd88cf57c7a8
arifinoid <rohmadarifin007@gmail.com>
2020-04-20 02:08:00 +0700

    Add more lessons

---
+++ loop.py
@@ -0,0 +1,12 @@
+i = 1
+colours = ['red', 'green', 'blue']
+
+while i <= 10:
+    print(i)
+    i = i + 1
+
+for colour in colours:
+    print(colour)
+
+for index in range(len(colours)):
+    print(colours[index])
