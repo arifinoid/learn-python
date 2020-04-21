e0f35e8094fb4fa24c2fcd7d7b341510539e4b2e
arifinoid <rohmadarifin007@gmail.com>
2020-04-21 19:52:29 +0700

    Add inheritance

---
+++ JavaneseChef.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+
+class JavaneseChef(Chef):
+    def make_lemper(self):
+        print('The cef makes special lemper')
+
+myChef = JavaneseChef()
+myChef.make_special_dish()
+myChef.make_lemper()
